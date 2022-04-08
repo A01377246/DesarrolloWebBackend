294707c6a8ffba88ee20aa41aa52f83f06324d6e
Humberto Carrillo <a01377246@itesm.mx>
2022-04-07 21:09:51 -0500

    Login y Logout con API

--- app.py
+++ app.py
@@ -1,13 +1,65 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request, session, redirect, url_for
+
+import datetime 
+
 
 # FlASK
 #############################################################
 app = Flask(__name__)
+
+#Generar sesión de un año
+app.permanent_session_lifetime = datetime.timedelta(days=365)
+app.secret_key = "super secret key"
+
 #############################################################
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    email = None
 
-def greet():
-    return "Nice to meet you"
+
+    if "email" in session:
+        email = session["email"]
+        return render_template('index.html', error = email)
+    else:
+        return render_template('login.html', error = email)
+
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    email = None
+    if "email" in session:
+        return render_template("index.html", error = email)
+    else:
+
+        if(request.method == "GET"):
+            return render_template("login.html", error= email)
+        else:
+            email = request.form["email"]
+            password = request.form["password"]
+            session["email"] = email
+            return render_template("index.html", error = email)
+
+@app.route("/prueba")
+def prueba():
+    nombres = []
+    nombres.append({"nombre": "ruben",
+    "Semestre01": [{
+        "matematicas": "10",
+        "español": "10"
+    }],
+    "Semestre02":[{
+        "matemáticas":"10",
+        "español": "10"
+    }]})
+
+    nombres.append({"nombre": "Sergio"})
+
+    return render_template("home.html", data = nombres)
+
+@app.route("/logout")
+def logout():
+    if "email" in session:
+        session.clear()
+        return redirect(url_for("home"))
